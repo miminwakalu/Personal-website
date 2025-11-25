7ab75ab44c47cb0b8eac35125aefdf84ae3bee47
Miracle Kalu <miminwakalu@gmail.com>
2025-11-25 18:30:08 +0100

    Made craps game and container responsive

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -14,37 +14,39 @@ const crapsStatsMoney = "craps-stats-money"
 const crapsStatsRounds = "craps-stats-rounds"
 
 function registerCrapsPlayer() {
-  crapsUsername = document.getElementById("craps-username-input"). value
+  crapsUsername = document.getElementById(crapsUsernameInput).value
 
-// Username validation check
-  let firstCharIsDigitRegex = /^[0-9]/g | /[^a-zA-Z0-9_]/g
-  if (crapsUsername.length < 5 || firstCharIsDigitRegex.test(crapsUsername)) {
-  alert("Username must be at least 5 characters long, alphanumeric and underscore only, no spaces,and cannot start with a number.")
-  } else {
+  // Username validation
+  let usernameRegex = /^(?![0-9])[A-Za-z0-9_]{5,}$/
+
+  if (!usernameRegex.test(crapsUsername)) {
+    alert("Username must be at least 5 characters long, alphanumeric and underscore only, no spaces, and cannot start with a number.")
+    return
+  }
+
   removeRegistrationPane()
   showMainGameSection()
   setupFirstRound()
-  }
 }
 
-function removeRegistrationPane () {
-document.gotElementById(crapsRegistrationPane).style.display = "none"
+function removeRegistrationPane() {
+  document.getElementById(crapsRegistrationPane).style.display = "none"
 }
 
-function showMainGameSection () {
-document.gotElementById(crapsMainSection).style.display = "block"
+function showMainGameSection() {
+  document.getElementById(crapsMainSection).style.display = "block"
 }
 
 function setupFirstRound() {
-document.getElementById(crapsStatsUsername).innerText = crapsUsername
-setMoney(startingMoney)
-setRounds(startingRounds)
+  document.getElementById(crapsStatsUsername).innerText = crapsUsername
+  setMoney(crapsStartingMoney)
+  setRounds(startingRounds)
 }
 
-function setMoney (money) {
-document.getElementById(crapsStatsMoney).innerText = money
+function setMoney(money) {
+  document.getElementById(crapsStatsMoney).innerText = money
 }
 
-function setRounds (rounds) {
-document.getElementById(crapsStatsRounds).innerText = rounds
+function setRounds(rounds) {
+  document.getElementById(crapsStatsRounds).innerText = rounds
 }
