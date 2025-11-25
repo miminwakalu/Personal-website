76cd479549fa244bf2311772898ec9734b375a15
Miracle Kalu <miminwakalu@gmail.com>
2025-11-25 01:42:33 +0100

    Added regex username validation to craps game

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -5,9 +5,15 @@ const crapsMainSection = "craps-main-section"
 
 function registerCrapsPlayer() {
   let crapsUsername = document.getElementById("craps-username-input"). value
-alert("Got: " + crapsUsername)
-removeRegistrationPane()
-showMainGameSection()
+
+// Username validation check
+  let firstCharIsDigitRegex = /^[0-9]/g | /[^a-zA-Z0-9_]/g
+  if (crapsUsername.length < 5 || firstCharIsDigitRegex.test(crapsUsername)) {
+  alert("Username must be at least 5 characters long, alphanumeric and underscore only, no spaces,and cannot start with a number.")
+  } else {
+  removeRegistrationPane()
+  showMainGameSection()
+  }
 }
 
 function removeRegistrationPane () {
