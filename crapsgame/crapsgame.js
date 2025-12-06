f0cbd9cbf0514dd8b3fdc0413c11ec76329d3071
Miracle Kalu <miminwakalu@gmail.com>
2025-12-06 03:20:59 +0100

    Implemented round finish grid

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -20,6 +20,9 @@ const crapsStatsRounds = "craps-stats-rounds"
 const crapsUserBetAmount = "craps-user-bet-amount"
 const crapsRollDiceButton = "craps-roll-dice-button"
 const crapsRollDiceAnimationContainer = "craps-roll-dice-animation-container";
+const crapsBettingGridContainer = "craps-betting-grid-container"
+const crapsRoundFinishGridContainer = "craps-round-finish-grid-container"
+const crapsRoundFinishMessage = "craps-round-finish-message"
 
 
 // In-game variables
@@ -54,6 +57,7 @@ function showMainGameSection() {
 }
 
 function setupFirstRound() {
+  document.getElementById(crapsRoundFinishGridContainer).style.display = "none"
   document.getElementById(crapsStatsUsername).innerHTML = crapsUsername
   setMoney(crapsStartingMoney)
   setRounds(startingRounds)
@@ -172,7 +176,7 @@ function rollDice() {
     processDiceResult(diceResults);
 }
 function delayedProcessDiceResult (diceResult) {
-  setTimeout(function() { processDiceResult(diceResult) }, 1000)
+  setTimeout(function() { processDiceResult(diceResult) }, 1800)
 }
 function processDiceResult(diceResult) {
   const sum = diceResult.reduce((partialSum, a) => partialSum + a, 0)
@@ -182,11 +186,18 @@ function processDiceResult(diceResult) {
       diceSumResult = Bets.odd;
   }
   setRounds(currentRounds + 1)
+  let roundFinishMessage = ""
   if (diceSumResult === currentBet) {
-      alert("YOU WIN!");
+    roundFinishMessage = "YOU WIN!"
     setMoney(currentMoney + currentBetAmount)
   } else {
-      alert("YOU LOSE");
-
+    roundFinishMessage = "YOU LOSE :("
+    setMoney(currentMoney - currentBetAmount)
   }
+  if (currentMoney === 0) {
+    roundFinishMessage = "YOU'RE OUT!"
+  }
+  document.getElementById(crapsBettingGridContainer).style.display = "none"
+  document.getElementById(crapsRoundFinishGridContainer).style.display = "block"
+  document.getElementById(crapsRoundFinishMessage).innerHTML = roundFinishMessage
 }
